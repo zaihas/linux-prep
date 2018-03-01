92e71bdf9d8c421f763ebc0d0c7714a3e1a80dbf
Zaihas Amri Fahdzan Hasfar <zaihas.amri@gmail.com>
2018-03-02 03:17:12 +0800

    add gcc arm v7.0 x64

--- env-modules.sh
+++ env-modules.sh
@@ -15,16 +15,21 @@ PATH_TO_CC_TGT=/opt
 PATH_TO_PRI_MOD=/opt/module
 
 # list of compiler
-declare -a CC_VER=("gcc-arm-5.4")
+declare -a CC_VER=( "gcc-arm-5.4"\
+                    "gcc-arm-7.0"\
+                   )
 
 # list of GCC compiler tarball
-declare -a CC_TAR_FILE=("gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2")
+declare -a CC_TAR_FILE=( "gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2" \
+                         "gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2"     \
+                        )
 
 # source of tarball - url
-declare -a CC_URL=("https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download")
+declare -a CC_URL=( "https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download" \
+                    "https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4"     \
+                   )
 
 NUM_CC=${#CC_VER[@]}
-
 
 # start installation
 # -----------------------------------------------------------
@@ -54,14 +59,19 @@ sudo mkdir -p $PATH_TO_PRI_MOD
 
 for ((idx=0; idx<$NUM_CC; idx++)); do
 
-    sudo mkdir -p $PATH_TO_CC_TGT/${CC_VER[$i]}
+    sudo mkdir -p $PATH_TO_CC_TGT/${CC_VER[$idx]}
 
     # get fw and extract to target folder - /opt/<compiler_name>
-    wget ${CC_URL[$i]}/${CC_TAR_FILE[$i]} -P $PATH_TO_CC_TAR
-    sudo tar xvjf $PATH_TO_CC_TAR/${CC_TAR_FILE[$i]} -C $PATH_TO_CC_TGT/${CC_VER[$i]} --strip-components=1
+    wget ${CC_URL[$idx]}/${CC_TAR_FILE[$idx]} -P $PATH_TO_CC_TAR
+    sudo tar xvjf $PATH_TO_CC_TAR/${CC_TAR_FILE[$idx]} -C $PATH_TO_CC_TGT/${CC_VER[$idx]} --strip-components=1
 
     # copy config for that module
-    sudo cp $PATH_TO_CC_MOD_CFG/${CC_VER[$i]} $PATH_TO_PRI_MOD
+    sudo cp $PATH_TO_CC_MOD_CFG/${CC_VER[$idx]} $PATH_TO_PRI_MOD
+
+    # remove the target temp folder
+    if [ -d "$PATH_TO_CC_TAR" ]; then
+        rm -rf $PATH_TO_CC_TAR
+    fi
 done
 
 # re-initialize terminal with new setting
